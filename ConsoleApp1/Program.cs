f9bd272156333bb8b4982de7c2675d80126c4566
Daniil <dadyarri@gmail.com>
2022-07-02 18:31:32 +0300

    add some examples of tags to docs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,18 +7,37 @@
     {
         /// <summary>
         /// Точка входа. Вызывает Program.SomeMethodWithParams.
+        /// @todo Добавить переключатель темы
+        /// @warning some warning
         /// </summary>
         public static void Main()
         {
             var res = SomeMethodWithParams(true);
+            var res2 = SomeMethodWithParams(2);
         }
 
         /// <summary>
         /// Метод-заглушка, возвращающий переданный параметр.
+        /// @since 1.0
+        /// @bug some ugly bug
+        /// @throws ArgumentException ...
         /// </summary>
         /// <param name="param">Просто какой-то параметр.</param>
         /// <returns>Переданный параметр без изменений.</returns>
         public static bool SomeMethodWithParams(bool param)
+        {
+            if (param)
+                return param;
+            throw new ArgumentException("Аргумент не может быть ложным");
+        }
+
+        /// <summary>
+        /// Метод-заглушка, возвращающий переданный параметр.
+        /// @since 1.0
+        /// </summary>
+        /// <param name="param">Целочисленный параметр.</param>
+        /// <returns>Переданный параметр без изменений.</returns>
+        public static int SomeMethodWithParams(int param)
         {
             return param;
         }
