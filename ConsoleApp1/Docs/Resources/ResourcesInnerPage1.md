d228b0a22251c2e1b9643193cebd1486416d2c47
Daniil <dadyarri@gmail.com>
2022-07-02 18:07:17 +0300

    update docs

---
+++ ConsoleApp1/Docs/Resources/ResourcesInnerPage1.md
@@ -0,0 +1,3 @@
+# Inner page in resources {#ResourcesInnerPage1}
+
+some content for inner page #1 in resources
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,26 @@
 ﻿namespace ConsoleApp1
 {
     /// <summary>
-    /// Some simple class
+    /// Простой класс
     /// </summary>
     public class Program
     {
         /// <summary>
-        /// Entrypoint
+        /// Точка входа. Вызывает Program.SomeMethodWithParams.
         /// </summary>
         public static void Main()
         {
-            
+            var res = SomeMethodWithParams(true);
+        }
+
+        /// <summary>
+        /// Метод-заглушка, возвращающий переданный параметр.
+        /// </summary>
+        /// <param name="param">Просто какой-то параметр.</param>
+        /// <returns>Переданный параметр без изменений.</returns>
+        public static bool SomeMethodWithParams(bool param)
+        {
+            return param;
         }
     }   
 }
